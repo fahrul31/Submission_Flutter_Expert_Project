6c90575902cfe34f4e98e47fa37a64d88223a820
Muh. Fachrul Hidayat <fahrulhidayat73@gmail.com>
2024-03-29 20:15:34 +0800

    Add first workflow

--- movie/test/json_reader.dart
+++ movie/test/json_reader.dart
@@ -3,7 +3,8 @@ import 'dart:io';
 String readJson(String name) {
   var dir = Directory.current.path;
   if (dir.endsWith('/test')) {
-    dir = dir.replaceAll('./movie/test', '');
+    dir = dir.replaceAll('/test', '');
   }
+  print('$dir/test/$name');
   return File('$dir/test/$name').readAsStringSync();
 }
--- tv_series/test/json_reader.dart
+++ tv_series/test/json_reader.dart
@@ -3,8 +3,7 @@ import 'dart:io';
 String readJson(String name) {
   var dir = Directory.current.path;
   if (dir.endsWith('/test')) {
-    dir = dir.replaceAll('./tv_series/test', '');
+    dir = dir.replaceAll('/test', '');
   }
-  print('$dir/test/$name');
   return File('$dir/test/$name').readAsStringSync();
 }
