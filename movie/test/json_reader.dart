6ee848208592cbc42bcf6cd288d4d38f479fbd5c
Muh. Fachrul Hidayat <fahrulhidayat73@gmail.com>
2024-03-29 20:38:24 +0800

    update json_reader.dart

--- movie/test/json_reader.dart
+++ movie/test/json_reader.dart
@@ -2,9 +2,11 @@ import 'dart:io';
 
 String readJson(String name) {
   var dir = Directory.current.path;
-  if (dir.endsWith('/test')) {
-    dir = dir.replaceAll('/test', '');
+  if (dir.endsWith('test')) {
+    return File('$dir/$name').readAsStringSync();
+  } else if (dir.endsWith('movie')) {
+    return File('$dir/test/$name').readAsStringSync();
+  } else {
+    return File('$dir/movie/test/$name').readAsStringSync();
   }
-  print('$dir/test/$name');
-  return File('$dir/test/$name').readAsStringSync();
 }
--- tv_series/test/json_reader.dart
+++ tv_series/test/json_reader.dart
@@ -2,8 +2,11 @@ import 'dart:io';
 
 String readJson(String name) {
   var dir = Directory.current.path;
-  if (dir.endsWith('/test')) {
-    dir = dir.replaceAll('/test', '');
+  if (dir.endsWith('test')) {
+    return File('$dir/$name').readAsStringSync();
+  } else if (dir.endsWith('tv_series')) {
+    return File('$dir/test/$name').readAsStringSync();
+  } else {
+    return File('$dir/tv_series/test/$name').readAsStringSync();
   }
-  return File('$dir/test/$name').readAsStringSync();
 }
